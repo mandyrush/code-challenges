305ecf0d17e395fb5039420b8664b5447beefc38
mandyrush <mandyrush85@gmail.com>
2022-02-08 20:29:08 -0700

    FizzBuzz

--- app.js
+++ app.js
@@ -13,12 +13,49 @@ for (i = 0; i < 7; i++) {
         hashString += '#';
     }
     console.log(hashString);
-    hashString = '';
 }
 
 // Book Answer
-for (let line = "#"; line.length < 8; line += "#")
-    console.log(line);
+// for (let line = "#"; line.length < 8; line += "#")
+//     console.log(line);
 
 
 
+// Fizzbuzz
+
+// Print all of the numbers from 1 to 100 with 2 exceptions
+// Numbers divisible by 3, print "Fizz"
+// Numbers divisible by 5 (and not 3), print "Buzz"
+// Numbers divisible by 3 and 5, print "FizzBuzz"
+
+
+// My Answer
+// Create a loop from 1 - 100 (inclusive)
+// On each loop check to see if the current number is divisible by 3 and 5 using modulo
+// If it is, print "FizzBuzz"
+// Otherwise check to see if the current number is divisible by 3 using modulo
+// If it is, print "Fizz"
+// Otherwise check to see if the current number is divisible by 5 using modulo
+// If it is, print "Buzz"
+// Otherwise, print the number
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
+
+
+// Book Answer
+// for (let n = 1; n <= 100; n++) {
+//     let output = "";
+//     if (n % 3 == 0) output += "Fizz";
+//     if (n % 5 == 0) output += "Buzz";
+//     console.log(output || n);
+// }
